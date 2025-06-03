1abea69983e040e4cf514c3fbb2e58de75abdbfc
azizbek1701 <azizbekkamilov1701@gmail.com>
2025-06-03 15:15:49 +0500

    courses list

---
+++ src/Pages/courses_list.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import Coures_img from '../assets/course.png';
+import PDP_logo from '../assets/pdp_logo.png';
+
+export default class courses_list extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div>
+                        <div>
+                            <h3>Saralash</h3>
+                            <a href="#">Tozzalash</a> <hr />
+                        </div>
+                        <div>
+                            <h4>Kurslar narx oralig’i</h4>
+                            <input type="radio" name="" id="" />
+                        </div>
+                        <div>
+                            <p>2,500,000</p>
+                            <p>2,500,000</p>
+                        </div>
+                        <div>
+                            <p>O’quv markaz bo’yicha</p>
+                            <input type="search" name="" id="" placeholder='O’quv markaz nomi' />
+
+                            <div>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p>O’quv kurs yo’nalishi</p>
+                            <input type="search" name="" id="" placeholder='O’quv kurs ' />
+
+                            <div>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                                <input type="checkbox" name="" id="" /> <p>PDP ACADEMY</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p>Kurslar davomiyligi</p>
+
+                            <div>
+                                <input type="checkbox" name="" id="" /> <p>1 oygacha</p>
+                                <input type="checkbox" name="" id="" /> <p>1 oygacha</p>
+                                <input type="checkbox" name="" id="" /> <p>1 oygacha</p>
+                                <input type="checkbox" name="" id="" /> <p>1 oygacha</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p>Kurslar formati</p>
+
+                            <div>
+                                <input type="checkbox" name="" id="" /> <p>Onlayn</p>
+                                <input type="checkbox" name="" id="" /> <p>Onlayn</p>
+                                <input type="checkbox" name="" id="" /> <p>Onlayn</p>
+                                <input type="checkbox" name="" id="" /> <p>Onlayn</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <input type="search" name="" id="" placeholder='O’quv kurs nomi' />
+
+                            <select name="select" id="">
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                                <option value="Hudud">Hudud</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p>Kurslar soni: 77</p>
+
+                            <div>
+                                <div>
+                                    <img src={Coures_img} alt="" />
+                                    <div>
+                                        <img src={PDP_logo} alt="" />
+                                        <p>PDP Academy</p>
+                                    </div>
+
+                                    <div>
+                                        <p>Create beautiful websites from
+                                            the scratch </p>
+                                            <p>900,000 Uzs/oyiga</p>
+                                    </div>
+                                    <div>
+                                        <p>Tashkent</p>
+                                        <p>Tashkent</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
